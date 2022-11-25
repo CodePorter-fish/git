0876316946b1e5534a3dfb9461808c6a5efae617
xq <xqing1010@163.com>
2022-11-25 10:05:25 +0800

    master-5

--- src/com/xq/Main.java
+++ src/com/xq/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("第二个版本");
         System.out.println("master冲突合并分支1");
         System.out.println("hot-fix冲突合并分支");
+        System.out.println("推送测试");
     }
 }
