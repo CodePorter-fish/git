bf2db2f195ef2a142a7eedd4ead0449f41fc3bd6
xq <xqing1010@163.com>
2022-11-25 09:08:01 +0800

    master-4

--- src/com/xq/Main.java
+++ src/com/xq/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("第二个版本");
-        System.out.println("master冲突合并分支");
+        System.out.println("master冲突合并分支1");
     }
 }
